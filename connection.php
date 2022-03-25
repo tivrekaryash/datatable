be25cf052a85ef5be9a9ec0a9ab65f8149d6a0b1
tivrekaryash <tivrekaryash@gmail.com>
2022-03-26 02:15:50 +0530

    new update

--- connection.php
+++ connection.php
@@ -1,6 +1,15 @@
 <?php
-$con  = mysqli_connect('localhost','root','','hrms_proto_db');
-if(mysqli_connect_errno())
+
+// file to set connection
+$servername ="localhost";
+$username = "root";
+$password = "";
+$dbname = "hrms_proto_db";
+
+$con = new mysqli($servername, $username, $password, $dbname);
+
+if($con->connect_error)
 {
-    echo 'Database Connection Error';
+    die("connection failed: ". $con->connect_error  );
 }
+?>
